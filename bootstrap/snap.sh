4b29400efd9966589e508409dc33da1a83ee6f60
Keita Ichihashi <ktng.attc.19@gmail.com>
2024-10-31 22:13:22 +0900

    Update

--- bootstrap/snap.sh
+++ bootstrap/snap.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-# Jetbrains IDE
-sudo snap install intellij-idea-ultimate --classic
-sudo snap install pycharm-professional --classic
-sudo snap install clion --classic
-sudo snap install goland --classic
-
-# Neovim
-sudo snap install nvim --classic
-
-# Visual Studio Code
 sudo snap install code --classic
+sudo snap install nvim --classic
+sudo snap install intellij-idea-community --classic
 
 exit 0
